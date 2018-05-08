94d613a83f1a3b8fe1745e7b025b7616eacad186
张高培 <m15639160073@163.com>
2018-05-08 10:42:23 +0800

    新增用户修改接口

--- src/main/java/com/iot/quickhpu/web/UserController.java
+++ src/main/java/com/iot/quickhpu/web/UserController.java
@@ -4,8 +4,6 @@ import com.iot.quickhpu.common.HPUResult;
 import com.iot.quickhpu.common.JsonUtils;
 import com.iot.quickhpu.pojo.User;
 import com.iot.quickhpu.util.FileUtil;
-import org.apache.commons.io.FileUtils;
-import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -126,7 +124,7 @@ public class UserController {
             }
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
 
         return new HPUResult(500, "用户不存在", null);
@@ -141,13 +139,37 @@ public class UserController {
      */
     @RequestMapping("update/{createId}")
     @ResponseBody
-    public HPUResult updateUser(@PathVariable String createId) {
-        // 读取user.json文件
-        // 转为list集合
-        // 遍历list集合
-        // 如果用户存在, 更新
-        // 如果用户不存在, 提示错误信息
-        return HPUResult.ok();
+    public HPUResult updateUser(@PathVariable String createId, @RequestBody User user) {
+        try {
+            String fileName = createId + ".json";
+            // 如果文件不存在, 则提示错误信息
+            if (!FileUtil.isExist(USER_JSON_DIR_PATH, fileName)) {
+                return new HPUResult(500, "修改用户失败, IO流异常", null);
+            }
+            // 读取user.json文件
+            String json = FileUtil.readFromFile(USER_JSON_DIR_PATH + fileName);
+            // 转化为list集合
+            List<User> userList = JsonUtils.jsonToList(json, User.class);
+            if (userList != null) {
+                // 遍历list集合
+                for (int i = 0; i < userList.size(); i++) {
+                    User retainUser = userList.get(i);
+                    String studentId = retainUser.getStudentId();
+                    if (studentId != null && studentId.equals(user.getStudentId())) {
+                        // 修改信息
+                        userList.set(i,user);
+                        return HPUResult.ok(userList);
+                    }
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new HPUResult(500, "用户不存在", null);
+
     }
 
     /**
